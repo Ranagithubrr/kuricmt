a9b26a6e0c47e5e7c422b98e3544c3f2e063eacc
ranagithubrr <mr6430119@gmail.com>
2023-11-28 10:45:07 +0600

    Feat: Error warning setup in login page

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import SigninImg from '../../img/login.svg';
 import { useSelector, useDispatch } from 'react-redux';
+import { IoIosWarning } from 'react-icons/io'
 import axios from 'axios';
 import { setUser } from '../../redux/userReducer/userActions';
-import {useNavigate} from 'react-router'
+import { useNavigate } from 'react-router'
 
 const Login = () => {
     const navigate = useNavigate();
@@ -13,37 +14,68 @@ const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [errorText, setErrorText] = useState("");
+
 
     const LoginClicked = async () => {
-        setLoading(true);
+        const clearError = () => {
+            setError(false);
+            setErrorText("");
+        };    
         if (email === "" || password === "") {
-            return window.alert("Please provide email and password")
+            setError(true);
+            setErrorText("Please Fill all the Fields");
+            setTimeout(clearError, 3000);
+            return;
         }
-        axios.post('https://kuricmt.onrender.com/user/login', {
-            email,
-            password
-        })
-            .then(function (response) {
-                console.log(response.data.user);
+
+        try {
+            setLoading(true);
+            const response = await axios.post('https://kuricmt.onrender.com/user/login', {
+                email,
+                password
+            });
+
+            if (response.status !== 200) {
+                setError(true);
+                setErrorText(response.status === 404 ? "Invalid Credentials" : "Server Error");
+                setTimeout(clearError, 3000);
+            } else {
+                setError(false);
                 response.data.msg = undefined;
                 dispatch(setUser(response.data));
                 setLoading(false);
-                navigate('/dashboard')
-            })
-            .catch((err) => {
-                setLoading(false)
-                console.log(err)
-            })
+                navigate('/dashboard');
+            }
+        } catch (err) {
+            setLoading(false);
+            setError(true);
+            setErrorText("Invalid Credentials");
+            console.error(err);
+            setTimeout(clearError, 3000);
+        }
 
-            console.log(userState)
-    };      
+        console.log(userState);
+    };
     return (
         <div className="w-2/3 mx-auto h-screen items-center flex bg-white">
             <div className="w-1/2">
                 <img src={SigninImg} alt="" />
-            </div>            
+            </div>
             <div className="w-1/2">
                 <div className='w-2/3 ml-10'>
+                    {
+                        error &&
+                        <div className='flex items-center bg-yellow-200 px-4 py-3'>
+                            <div className='w-2/12'>
+                                <span className='text-yellow-600 text-4xl'><IoIosWarning /></span>
+                            </div>
+                            <div className='w-10/12'>
+                                <span className='font-semibold text-sm'>{errorText}</span>
+                            </div>
+                        </div>
+                    }
                     <h3 className='text-blue-500 font-sans font-bold text-xl mb-2'>Login Account</h3>
                     <input type="email" onChange={(e) => setEmail(e.target.value)} placeholder='Email ID' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
                     <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder='Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
