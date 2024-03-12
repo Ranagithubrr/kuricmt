b4eb3cbd3cd93974c9be2eec3f08dd0055fca992
ranagithubrr <mr6430119@gmail.com>
2024-03-13 02:35:48 +0600

    Feat notice upload done

--- src/pages/Notifications/Notification/Notification.jsx
+++ src/pages/Notifications/Notification/Notification.jsx
@@ -1,13 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { CiMenuKebab } from "react-icons/ci";
+import { FaFilePdf, FaImage } from "react-icons/fa";
 
-const Notification = () => {
+const Notification = ({ item }) => {
+    const dateString = item.createdAt;
+    const date = new Date(dateString);
+
+    const formattedDate = `${date.toLocaleDateString()} at ${date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' })}`;
     return (
         <div className='bg-gray-100 dark:bg-gray-800 shadow p-4 rounded my-2'>
-            <h4 className='font-semibold dark:text-gray-200'>Notification title</h4>
             <div className='flex justify-between'>
-                <p className='text-sm text-gray-700 dark:text-gray-300 text-justify pr-4'>Notification descriptions! </p>
-                <span className='text-sm font-semibold dark:text-gray-300'>23/08/2023 at 11:30 AM</span>
+                <h4 className='font-semibold dark:text-gray-200'>{item.title}</h4>
+                <div className='flex items-center'>
+                    <a href={item.noticeurl} download={item.noticeurl} target='_blank' rel="noreferrer">
+                    <FaFilePdf className='text-red-500 text-3xl cursor-pointer mr-3'/>
+                    </a>
+                    {/* <FaImage className='text-blue-500 text-3xl cursor-pointer mr-3'  /> */}
+                    <CiMenuKebab className='text-black text-md cursor-pointer' />
+                </div>
             </div>
+            <div className='flex justify-between'>
+                <p className='text-sm text-gray-700 dark:text-gray-300 text-justify pr-4'>{item.description}</p>
+                <span className='text-sm font-semibold dark:text-gray-300'>{formattedDate}</span>
+            </div>           
         </div>
     )
 }
--- src/pages/Notifications/Notifications.jsx
+++ src/pages/Notifications/Notifications.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Notification from './Notification/Notification'
+import axios from 'axios';
 
 const Notifications = () => {
+  const [notices, setNotices] = useState([]);
+  console.log(notices)
+  const FetchNotices = async () => {
+    const notices = await axios.get("http://localhost:4000/notice");
+    setNotices(notices.data.AllNotices)
+  }
+  useEffect(() => {
+    FetchNotices()
+  }, [])
   return (
     <div className='w-1/2 m-auto'>
-      <h4 className='font-semibold text-xl m-3 ml-0'>Notifications</h4>
-      <Notification />
-      <Notification />
-      <Notification />
+      <h4 className='font-semibold text-xl m-3 ml-0'>Notices</h4>           
+      {
+        notices && notices.map((ele) => {
+          return (
+            <Notification item={ele}/>
+          )
+        })
+      }
     </div>
   )
 }
