8fd9b03d7c8bb6f54ed76ecd2dae092b2a8565c2
ranagithubrr <mr6430119@gmail.com>
2024-05-25 10:57:17 +0600

    Fix: sidebar bg color fixed

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -22,7 +22,7 @@ const Sidebar = () => {
     }
     return (
         <>
-            <div className={`${!sidebar ? 'w-8/12 lg:w-3/12' : 'w-[6rem] -left-full lg:left-0'} z-50 top-20 fixed lg:sticky transition-all ease-in-out overflow-y-scroll pb-10 dark:border-gray-700 border-r shadow-lg dark:shadow-transparent dark:bg-slate-800`} style={{ height: '85vh', }}>
+            <div className={`${!sidebar ? 'w-8/12 lg:w-3/12' : 'w-[6rem] -left-full lg:left-0'} z-50 top-20 fixed lg:sticky transition-all ease-in-out overflow-y-scroll pb-10 dark:border-gray-700 border-r shadow-lg dark:shadow-transparent bg-white dark:bg-slate-800`} style={{ height: '85vh', }}>
                 <div className='w-full text-center flex justify-end pr-7 pt-4 dark:text-gray-200 font-bold text-2xl cursor-pointer'>
                     <span className='' onClick={() => setSidebar((prev) => !prev)}><HiOutlineBars3BottomRight /> </span>
                 </div>
