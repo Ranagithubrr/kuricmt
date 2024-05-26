edc08ab6ef96ec40d14677be03e8c9234e40e114
ranagithubrr <mr6430119@gmail.com>
2024-05-26 13:43:05 +0600

    Fix: review dark mode complete

--- src/components/dashboard/review/Review.jsx
+++ src/components/dashboard/review/Review.jsx
@@ -1,31 +1,33 @@
 import React from 'react';
 import Singlereview from './Singlereview';
+import { Rating } from '@material-tailwind/react';
 
 const Review = () => {
     // const [value, setValue] = useState(4.6);    
     return (
         <div className='pl-10 pt-5'>
-            <span className='bg-red-400 px-5 rounded-sm text-gray-100'>Demo Page</span>
-            <h4 className='font-semibold text-xl'>Reviews</h4>
+            <span className='bg-red-400 px-5 rounded-sm text-gray-100 dark:bg-red-600 dark:text-gray-300'>Demo Page</span>
+            <h4 className='font-semibold text-xl dark:text-gray-300'>Reviews</h4>
             <div className='flex justify-between w-1/3 my-4'>
                 <div>
-                    <span className='block font-semibold'>Total Reviews</span>
-                    <span className='block font-semibold text-4xl'>360</span>
+                    <span className='block font-semibold dark:text-gray-300'>Total Reviews</span>
+                    <span className='block font-semibold text-4xl dark:text-gray-300'>360</span>
                 </div>
                 <div>
-                    <span className='block font-semibold'>Average Rating</span>
-                    <span className='font-semibold text-4xl mr-2'>4.9</span>
-                    
+                    <span className='block font-semibold dark:text-gray-300'>Average Rating</span>
+                    <Rating value={4} readonly className='flex text-yellow-400 my-2' />
+                    <span className='font-semibold text-4xl mr-2 dark:text-gray-300'>4.9</span>
                 </div>
             </div>
 
+
             <Singlereview />
             <Singlereview />
             <Singlereview />
             <Singlereview />
             <Singlereview />
 
-            <div className='my-5'>            
+            <div className='my-5'>
             </div>
         </div>
     );
--- src/components/dashboard/review/Singlereview.jsx
+++ src/components/dashboard/review/Singlereview.jsx
@@ -1,27 +1,25 @@
+import { Rating } from '@material-tailwind/react';
 import React from 'react';
-import Avatar from '../../../img/docc.png';
+
 
 const Singlereview = () => {
     return (
         <div className='flex my-10'>
-            <div>
-                <img src={Avatar} alt="profile img" className='h-20 w-20 bg-gray-300 rounded' />
-            </div>
             <div className='w-56 p-2'>
-                <h4 className='font-semibold m-0 p-0'>Masud Rana</h4>
-                <span className='text-sm text-gray-500 block'>Junior Instructor</span>                
-
+                <h4 className='font-semibold m-0 p-0 dark:text-gray-300'>Masud Rana</h4>
+                <span className='text-sm text-gray-500 block dark:text-gray-400'>Junior Instructor</span>
             </div>
             <div className='ml-2 w-6/12'>
+                <Rating value={4} readonly className='flex text-yellow-400 my-2' />
                 <div className='flex items-center'>
-                    
-                    <span className='text-sm font-semibold text-gray-500 ml-2'>24/08/2023</span>
+                    <span className='text-sm font-semibold text-gray-500 dark:text-gray-400'>24/08/2023</span>
                 </div>
                 <div>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam labore animi nesciunt mollitia delectus, accusamus error earum molestias voluptatum natus.</p>
+                    <p className='dark:text-gray-300'>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ullam labore animi nesciunt mollitia delectus, accusamus error earum molestias voluptatum natus.</p>
                 </div>
             </div>
         </div>
+
     )
 }
 
