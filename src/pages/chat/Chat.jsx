7f03957208a8a45ff4ef09a03158bc9706ba55d4
ranagithubrr <mr6430119@gmail.com>
2024-05-26 18:48:18 +0600

    Fix: removed user photo from message, fetched from user data

--- src/pages/chat/Chat.jsx
+++ src/pages/chat/Chat.jsx
@@ -1,16 +1,19 @@
 import React, { useState, useEffect, useRef } from 'react';
 import io from 'socket.io-client';
 import { useAuth } from '../../contexts/AuthContext';
+import axios from 'axios';
+import NotfoundImage from '../../img/no-profile.png';
 
 const Chat = () => {
     const [socket, setSocket] = useState(null);
     const [messages, setMessages] = useState([]);
     const [messageInput, setMessageInput] = useState('');
+    const [AllUsers, setUsers] = useState([]);
     const chatBoxRef = useRef(null);
 
     const { userData } = useAuth();
-    const { name, _id, image } = userData || {};
-    console.log(userData)
+    const { name, _id } = userData || {};
+    // console.log(userData)
     useEffect(() => {
         const newSocket = io('https://kuricmt-backend.onrender.com');
         setSocket(newSocket);
@@ -45,10 +48,10 @@ const Chat = () => {
         if (!socket || !messageInput.trim()) return;
 
         // Send a new message to the server
-        socket.emit('newMessage', { name, avatar: image, uid: _id, body: messageInput.trim(), });
+        socket.emit('newMessage', { name, uid: _id, body: messageInput.trim(), });
         setMessageInput('');
     };
-    console.log(messages);
+    // console.log(messages);
     const getTimeAgo = (messageTime) => {
         const currentTime = new Date();
         const messageDate = new Date(messageTime);
@@ -81,65 +84,89 @@ const Chat = () => {
             return messageDate.toLocaleDateString('en-US', options);
         }
     };
+
+    const FetchUsers = async () => {
+        try {
+            const res = await axios.get('https://kuricmt-backend.onrender.com/user');
+            setUsers(res.data.AllUser);
+        }
+        catch (err) {
+            console.log('err to fetch user')
+        }
+
+    }
+    useEffect(() => {
+        FetchUsers();
+    }, []);    
     return (
         <div className='mt-8'>
             <div>
                 <div className='mt-8'>
                     <div className="flex ">
                         <div className="w-1/5 px-2">
-                            <div className='bg-gray-200 px-3 py-4 rounded mt-2 cursor-pointer sticky top-0'>
-                                <span className='font-semibold'>Discussion</span>
+                            <div className='bg-gray-200 dark:bg-gray-700 px-3 py-4 rounded mt-2 cursor-pointer sticky top-0'>
+                                <span className='font-semibold dark:text-gray-200'>Discussion</span>
                             </div>
                         </div>
                         <div className="w-3/5">
-                            <div className="border px-2 relative h-96 overflow-hidden">
-                                <div className='bg-gray-200 px-3 py-4 rounded mt-2 cursor-pointer text-center  sticky top-0'>
-                                    <span className='font-semibold'>Chat</span>
+                            <div className="border px-2 relative h-96 overflow-hidden dark:border-gray-600">
+                                <div className='bg-gray-200 dark:bg-gray-700 px-3 py-4 rounded mt-2 cursor-pointer text-center  sticky top-0'>
+                                    <span className='font-semibold dark:text-gray-200'>Chat</span>
                                 </div>
                                 <div className='px-2 h-80 overflow-y-scroll pb-4' ref={chatBoxRef}>
-                                    {messages && messages.map((message, index) => (
-                                        message.uid !== _id ? (
-                                            <div className={`pr-20 flex items-center w-full`} key={index}>
+                                    {messages && messages.map((message, index) => {
+                                        const user = AllUsers.find(user => user._id === message.uid);
 
-                                                <div className='pr-2'>
-                                                    <img src={message.avatar} style={{minWidth:'30px', width:'30px', minHeight:'30px', height:'30px'}} className='h-10 w-10 rounded-full' alt="" />
-                                                </div>
-                                                <div>
-                                                    <div className="bg-gray-100 block px-3 py-2 rounded-lg rounded-bl-none my-2">
-                                                        <p className='text-sm font-semibold'>{message.body}</p>
+                                        if (user) {
+                                            if (message.uid !== _id) {
+                                                return (
+                                                    <div className='pr-20 flex items-center w-full' key={index}>
+                                                        <div className='pr-2'>
+                                                            <img src={user.image || NotfoundImage} style={{ minWidth: '30px', width: '30px', minHeight: '30px', height: '30px' }} className='h-10 w-10 rounded-full' alt="" />
+                                                        </div>
+                                                        <div>
+                                                            <div className="bg-gray-100 dark:bg-gray-800 block px-3 py-2 rounded-lg rounded-bl-none my-2">
+                                                                <p className='text-sm font-semibold dark:text-gray-300'>{message.body}</p>
+                                                            </div>
+                                                            <div>
+                                                                <span className='text-xs font-semibold text-gray-400 block'>{getTimeAgo(message.createdAt)}</span>
+                                                            </div>
+                                                        </div>
                                                     </div>
-                                                    <div>
-                                                        <span className='text-xs font-semibold text-gray-400 block'>{getTimeAgo(message.createdAt)}</span>
+                                                );
+                                            } else {
+                                                return (
+                                                    <div className='ml-20 flex justify-end items-center float-right w-full' key={index}>
+                                                        <div>
+                                                            <div className="bg-blue-400 block px-3 py-2 rounded-lg rounded-bl-none my-2">
+                                                                <p className='text-sm font-semibold'>{message.body}</p>
+                                                            </div>
+                                                            <div className='float-right'>
+                                                                <span className='text-xs font-semibold text-gray-400 block'>{getTimeAgo(message.createdAt)}</span>
+                                                            </div>
+                                                        </div>
+                                                        <div className='pl-2'>
+                                                            <img src={user.image || NotfoundImage} style={{ minWidth: '30px', width: '30px', minHeight: '30px', height: '30px' }} className='rounded-full' alt="" />
+                                                        </div>
                                                     </div>
-                                                </div>
-                                            </div>
-                                        ) : (
-                                            <div className='ml-20 flex justify-end items-center float-right w-full' key={index}>
-                                                <div>
-                                                    <div className="bg-blue-400 block px-3 py-2 rounded-lg rounded-bl-none my-2">
-                                                        <p className='text-sm font-semibold'>{message.body}</p>
-                                                    </div>
-                                                    <div className='float-right'>
-                                                        <span className='text-xs font-semibold text-gray-400 block'>{getTimeAgo(message.createdAt)}</span>
-                                                    </div>
-                                                </div>
-                                                <div className='pl-2'>
-                                                    <img src={message.avatar} style={{minWidth:'30px', width:'30px', minHeight:'30px', height:'30px'}} className='rounded-full' alt="" />
-                                                </div>
-                                            </div>
-                                        )
-                                    ))}
-                                    {
-                                        messages.length === 0 && <div className='flex items-center w-full justify-center pt-10'>
+                                                );
+                                            }
+                                        } else {
+                                            return null;
+                                        }
+                                    })}
+                                    {messages.length === 0 && (
+                                        <div className='flex items-center w-full justify-center pt-10'>
                                             <p className='text-gray-400 font-semibold text-sm'>No messages</p>
                                         </div>
-                                    }
+                                    )}
                                 </div>
+
                             </div>
                             <div className='flex mt-2'>
                                 <form onSubmit={sendMessage} className='flex w-full'>
-                                    <input type="text" value={messageInput} onChange={(e) => setMessageInput(e.target.value)} className='border rounded-sm w-10/12 text-sm px-3 py-2 outline-none mr-2' placeholder='Type Your Message . . .' />
-                                    <button type='submit' className='w-2/12 outline-none bg-green-600 rounded'>Send</button>
+                                    <input type="text" value={messageInput} onChange={(e) => setMessageInput(e.target.value)} className='border rounded-sm w-10/12 text-sm px-3 py-2 outline-none mr-2 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-600' placeholder='Type Your Message . . .' />
+                                    <button type='submit' className='w-2/12 outline-none bg-green-600 rounded font-semibold'>Send</button>
                                 </form>
                             </div>
                         </div>
