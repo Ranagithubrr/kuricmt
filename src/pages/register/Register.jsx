e7b5486aa3bb77adcb3cc868f1def2c0573df673
ranagithubrr <ranarr.dev@gmail.com>
2024-07-11 04:50:55 +0600

    Feat: email verification complete

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import SigninImg from '../../img/signup.svg';
 import axios from 'axios';
@@ -6,6 +6,7 @@ import { IoIosWarning } from 'react-icons/io';
 import { FaRegCheckCircle } from "react-icons/fa";
 import EmailImg from '../../img/password.png';
 import Verified from '../../img/verified.png';
+import Success from '../../img/check-mark.png';
 
 const Register = () => {
     const [name, setName] = useState("");
@@ -15,7 +16,50 @@ const Register = () => {
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [errorText, setErrorText] = useState("Something went wrong");
-    const [success, setSuccess] = useState(false);
+    const [step, setStep] = useState(0);
+    const [otp, setOtp] = useState('');
+    const [timeLeft, setTimeLeft] = useState(0);
+
+     // Function to start the countdown
+     const resetCountdown = () => {
+        setTimeLeft(120); // Reset to 2 minutes
+
+    };
+    const RegisterUser = async () => {
+        const clearError = () => {
+            setError(false);
+            setErrorText("");
+        };
+        try {
+
+            setLoading(true);
+            const response = await axios.post('https://kuricmt-backend.onrender.com/user/register', {
+                email,
+                password,
+                name
+            });
+            setStep(3)
+            setError(false);
+            response.data.msg = undefined;
+            setLoading(false);
+        } catch (err) {
+            console.log(err)
+            setLoading(false);
+            setError(true);
+            if (!err.response) {
+                return;
+            }
+            if (err.response.status === 400) {
+                setErrorText("Account with this email already exists, please log in")
+            } else if (err.response.status === 406) {
+                setErrorText("Account is not activated")
+            } else {
+                setErrorText("An error occured")
+            }
+            console.error(err);
+            setTimeout(clearError, 3000);
+        }
+    }
 
     const LoginClicked = async () => {
         const clearError = () => {
@@ -36,38 +80,92 @@ const Register = () => {
         }
 
         try {
-
             setLoading(true);
-            const response = await axios.post('https://kuricmt-backend.onrender.com/user/register', {
+            const response = await axios.post('https://kuricmt-backend.onrender.com/user/verify-email', {
                 email,
-                password,
                 name
             });
-            setError(false);
-            response.data.msg = undefined;
-            setLoading(false);
-            setSuccess(true)
+            if (response.status === 200) {
+                console.log('mail sent successfully');
+                setLoading(false);
+                setStep(1);
+                resetCountdown()
+            }
+
         } catch (err) {
-            console.log(err)
-            setLoading(false);
-            setError(true);
-            if (!err.response) {
-                return;
-            }
-            if (err.response.status === 400) {
+            if (err.response.status === 409) {
+                setError(true);
                 setErrorText("Account with this email already exists, please log in")
-            } else if (err.response.status === 406) {
-                setErrorText("Account is not activated")
+                setTimeout(clearError, 3000);
+
             } else {
-                setErrorText("An error occured")
+                console.log('an error occured');
             }
-            console.error(err);
-            setTimeout(clearError, 3000);
+            setLoading(false)
         }
     };
+    const VerifyOTP = async (e) => {
+        e.preventDefault();
+        const clearError = () => {
+            setError(false);
+            setErrorText("");
+        };
+        const notp = parseInt(otp)
+
+        try {
+            console.log('going otp is', otp);
+            const reponse = await axios.post('https://kuricmt-backend.onrender.com/user/verify-otp', {
+                email,
+                otp: notp
+            });
+            if (reponse.status === 200) {
+                setStep(2)
+                RegisterUser();
+                console.log('verified successfully');
+            }
+        }
+        catch (err) {
+            if (err.response.status === 504) {
+                setError(true);
+                setErrorText("Timed Out")
+                setTimeout(clearError, 3000);
+            } else {
+                setError(true);
+                setErrorText("Invalid OTP")
+                setTimeout(clearError, 3000);
+            }
+        }
+    }
+
+    // count down timer
+
+
+   
+
+    useEffect(() => {
+        if (timeLeft > 0) {
+            const timerId = setInterval(() => {
+                setTimeLeft((prevTime) => prevTime - 1);
+            }, 1000);
+
+            return () => clearInterval(timerId);
+        }else{
+            setOtp('')
+        }
+    }, [timeLeft]);
+
+    // Format time as MM:SS
+    const formatTime = (seconds) => {
+        const minutes = Math.floor(seconds / 60);
+        const remainingSeconds = seconds % 60;
+        return `${minutes.toString().padStart(2, '0')}:${remainingSeconds
+            .toString()
+            .padStart(2, '0')}`;
+    };
+
     return (
         <>
-            <div className=''>
+            {step === 0 && <div className=''>
                 <div className="w-full lg:w-2/3 lg:mx-auto h-screen items-center lg:flex  px-6 bg-white">
                     <div className="w-full lg:w-1/2 p-12 pt-20 lg:pt-0">
                         <img src={SigninImg} alt="" className='rotat ' style={{ transform: 'scaleX(-1)' }} />
@@ -85,30 +183,9 @@ const Register = () => {
                                     </div>
                                 </div>
                             }
-                            {
-                                success && <div>
-                                    <div className='flex items-center bg-green-200 px-4 py-3 my-3'>
-                                        <div className='w-2/12'>
-                                            <span className='text-green-600 text-4xl'><FaRegCheckCircle /></span>
-                                        </div>
-                                        <div className='w-10/12'>
-                                            <span className='font-semibold text-sm'>Congratullations, Your account created successfully.</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            }
-                            {
-                                success && <div>
-                                    <div className='flex items-center bg-yellow-300 px-4 py-3 my-1'>
-                                        <div className=''>
-                                            <span className='font-semibold text-sm'>You will be able to login after admin approve your account, Please Wait!</span>
-                                        </div>
-                                    </div>
-                                </div>
-                            }
                             <h3 className='text-blue-500 font-sans font-bold text-xl mb-2'>Create Account</h3>
                             <input value={name} onChange={(e) => setName(e.target.value)} type="text" placeholder='Your Full Name' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
-                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="text" placeholder='Email ID' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
+                            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder='Email ID' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
                             <input value={password} onChange={(e) => setPassword(e.target.value)} type="password" placeholder='Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
                             <input value={confirmpassword} onChange={(e) => setconfirmPassword(e.target.value)} type="password" placeholder='Confirm Password' className='outline-none bg-gray-100 px-4 py-1 border-l-2 w-full border-blue-500 my-2 block' />
                             <span className='text-sm font-semibold text-gray-500'> Already a Member ? <Link to="/login" className='text-blue-500'>Sign In</Link></span>
@@ -117,42 +194,101 @@ const Register = () => {
                     </div>
                 </div>
             </div>
-            <div className='hidden'>
-                <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-                    <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
-                        <div className='flex items-center justify-center py-4 flex-col'>
-                            <h2 className='font-semibold text-gray-900 text-2xl pb-4'>Verify Email</h2>
-                            <img src={EmailImg} alt="" className='h-24' />
-                            <p className='font-semibold text-gray-700'>A 6-digit OTP has been sent to your email.</p>
-                        </div>
-                        <h1 className="text-2xl font-bold mb-4">Enter OTP</h1>
-                        <form action="#">
-                            <div className="flex items-center justify-between gap-4">
-                                <input
-                                    type="text"
-                                    className="w-full tracking-widest h-12 text-center text-xl font-bold bg-gray-100 border border-transparent hover:border-gray-300 rounded px-3 outline-none focus:bg-white focus:ring-2 focus:ring-indigo-500"
-                                    maxLength="6"
-                                    inputMode="numeric"
-                                />
+            }
+            {step === 1 &&
+                <div>
+                    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+                        <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+                            {
+                                error &&
+                                <div className='flex items-center bg-red-200 px-4 py-3'>
+                                    <div className='w-2/12'>
+                                        <span className='text-red-600 text-4xl'><IoIosWarning /></span>
+                                    </div>
+                                    <div className='w-10/12'>
+                                        <span className='font-semibold text-sm'>{errorText}</span>
+                                    </div>
+                                </div>
+                            }
+                            <div className='flex items-center justify-center py-4 flex-col'>
+                                <h2 className='font-semibold text-gray-900 text-2xl pb-4'>Verify Email</h2>
+                                <img src={EmailImg} alt="" className='h-24' />
+                                <p className='font-semibold text-gray-700'>A 6-digit OTP has been sent to your email.</p>
                             </div>
-                            <button type="submit" className=" w-full mt-4 inline-block px-6 py-2 text-sm font-semibold text-center text-white uppercase transition-all duration-300 bg-gradient-to-r from-blue-500 to-blue-800 rounded-full shadow-md hover:from-blue-600 hover:to-blue-900 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-blue-300 active:shadow-inner">
-                                Verify
-                            </button>
-                        </form>
+                            <div className='flex justify-between items-center my-2'>
+                                {
+                                    timeLeft > 0 ?
+                                        <h1 className="text-2xl font-bold mb-4">Enter OTP</h1> :
+                                        <h1 className='text-2xl font-bold mb-4 text-red-700'>Expired</h1>
+                                }
+                                {
+                                    timeLeft > 0 ? <span className='text-sm'>Will Expire In {formatTime(timeLeft)}</span> : <div>
+                                        <button className='inline-block px-4 py-1 text-sm font-semibold text-center text-white uppercase transition-all duration-300 bg-gradient-to-r from-blue-500 to-blue-800 rounded-full shadow-md hover:from-blue-600 hover:to-blue-900 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-blue-300 active:shadow-inner' onClick={()=>LoginClicked()}>Resend OTP</button>
+                                    </div>
+                                }
+
+                            </div>
+                            <form onSubmit={VerifyOTP}>
+                                <div className="flex items-center justify-between gap-4">
+                                    <input
+                                        className="w-full tracking-widest h-12 text-center text-xl font-bold bg-gray-100 border border-transparent hover:border-gray-300 rounded px-3 outline-none focus:bg-white focus:ring-2 focus:ring-indigo-500"
+                                        type='text'
+                                        inputMode="numeric"
+                                        maxLength="6"
+                                        value={otp}
+                                        onInput={(e) => {
+                                            e.target.value = e.target.value.replace(/[^0-9]/g, '').slice(0, 6);
+                                        }}
+                                        onChange={(e) => {
+                                            setOtp(e.target.value)
+                                        }}
+                                    />
+
+                                </div>
+                                <button type="submit" className={` ${otp.length <= 5 ? 'hidden' : ''} w-full mt-4 inline-block px-6 py-2 text-sm font-semibold text-center text-white uppercase transition-all duration-300 bg-gradient-to-r from-blue-500 to-blue-800 rounded-full shadow-md hover:from-blue-600 hover:to-blue-900 hover:shadow-lg focus:outline-none focus:ring-4 focus:ring-blue-300 active:shadow-inner`}>
+                                    Verify
+                                </button>
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='hidden'>
-                <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
-                    <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
-                        <div className='flex items-center justify-center py-4 flex-col'>
-                            <h2 className='font-semibold text-gray-900 text-2xl pb-4'>Verified Successfully</h2>
-                            <img src={Verified} alt="" className='h-24' />
-                            <p className='font-semibold text-gray-700 pt-3'>Redirecting to Log In Page in 3s</p>
-                        </div>                       
+            }
+            {step === 2 &&
+                <div>
+                    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+                        <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+                            <div className='flex items-center justify-center py-4 flex-col'>
+                                <h2 className='font-semibold text-gray-900 text-2xl pb-4'>Verified Successfully</h2>
+                                <img src={Verified} alt="" className='h-24' />
+                                <p className='font-semibold text-gray-700 pt-3'>Redirecting in 3s</p>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
+            }
+            {step === 3 &&
+                <div>
+                    <div className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center bg-gray-500 bg-opacity-50">
+                        <div className="w-full max-w-md bg-white rounded-lg shadow-lg p-8">
+                            <img src={Success} className='h-16 mx-auto mb-8' alt="" />
+                            <div className='flex items-center bg-green-200 px-4 py-3 my-3'>
+                                <div className='w-2/12'>
+                                    <span className='text-green-600 text-4xl'><FaRegCheckCircle /></span>
+                                </div>
+                                <div className='w-10/12'>
+                                    <span className='font-semibold text-sm'>Congratullations, Your account created successfully.</span>
+                                </div>
+                            </div>
+                            <div className='flex items-center bg-yellow-300 px-4 py-3 my-1'>
+                                <div className=''>
+                                    <span className='font-semibold text-sm'>You will be able to login after admin approve your account, Please Wait!</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            }
         </>
     );
 };
